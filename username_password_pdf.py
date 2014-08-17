b52273ee8cc94b0e3b241851b7e977251e492b44
Babken Vardanyan <483ken@gmail.com>
2014-08-17 10:35:26 +0400

    added license

--- username_password_pdf.py
+++ username_password_pdf.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
+'''
+    username-password-pdf
+
+    Generates LaTeX reports containing a random password
+    and given username and opens the resulting PDF in system's
+    default PDF viewer
+
+    Copyright (C) 2014  Babken Vardanyan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 from __future__ import print_function
 import Tkinter
